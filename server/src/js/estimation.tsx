c4e380871cb1dedb2a6eeec8409dfe1786594b15
Ryan Posey <64180489+Bombzone@users.noreply.github.com>
2025-04-24 11:40:58 -0600

    Delete button (#30)
    * db
    
    * commit
    
    * commit
    
    * update
    
    * fix
    
    * update
    
    * fix
    
    * gitignore
    
    * Delete .vscode directory
    
    Signed-off-by: Ryan Posey <64180489+Bombzone@users.noreply.github.com>
    
    * Add Story modal
    
    * styles for add story
    
    * fix autocomplete
    
    * Delete Button
    
    * cleanup and revert storybutton styles
    
    ---------
    
    Signed-off-by: Ryan Posey <64180489+Bombzone@users.noreply.github.com>

--- server/src/js/estimation.tsx
+++ server/src/js/estimation.tsx
@@ -63,7 +63,7 @@ const CurrentQueue = (props: { storyQueue: UserStoryQueue; cards: Cards }) => { 
         <section id="currentQueue">
             <div>
                 <h3>Now estimating:</h3>
-                <h4><Story story={currentStory} list={false} /></h4>
+                <h4 className="deleteParent"><Story story={currentStory} list={false} /></h4>
             </div>
             <h4>AVG</h4>
             <ul>
@@ -105,9 +105,6 @@ const StQueue = (props: { storyQueue: UserStoryQueue }) => { // returns the stor
         let stories = []
         for (let index = 1; index < storyQueue.getLength() - 1; index++) {
             stories.push(<Story key={index} story={storyQueue.findAt(index)} list={true} />)
-            if (index == 1) {
-                stories.push(<li key={storyQueue.getLength() + 1}></li>)
-            }
         }
         stories.push(<StoryDialogBox key={0} />)
         // stories.push(<div key={0}><button id="storyButton" onClick={() => {
@@ -137,15 +134,19 @@ const Story = (props: { story: UserStory | undefined; list: boolean }) => { // r
         }
         if (props.list) {
             return (
-                <div>
+                <li className="deleteParent">
                     {story.toString()}
-                    <button id="liStory" onClick={deleteStory}>Delete</button>
-                </div>
+                    <button className="liStory" onClick={deleteStory}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+</svg></button>
+                </li>
             )
         } else {
             return (<>
                 {story.toString()}
-                <button id="currentDelete" onClick={deleteStory}>Delete</button>
+                <button id="currentDelete" onClick={deleteStory}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+</svg></button>
             </>)
         }
     }
@@ -201,7 +202,7 @@ const EstimationButton = (props: { card: Card }) => { // returns a single estima
         fetch.post("estimations", { value: props.card.getValue() })
     }
     return (
-        <li><button onClick={submit} value={props.card.getValue()}>{props.card.getValue()}</button></li>
+        <li><button className="estButton" onClick={submit} value={props.card.getValue()}>{props.card.getValue()}</button></li>
     )
 }
 export default Estimation;
