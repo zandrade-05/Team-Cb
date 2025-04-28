fb08721ed28e5756ee2f3113ec1859d66fcab941
Ryan Posey <64180489+Bombzone@users.noreply.github.com>
2025-04-28 13:27:26 -0600

    table (#33)
    * db
    
    * commit
    
    * commit
    
    * update
    
    * fix
    
    * update
    
    * fix
    
    * gitignore
    
    * Delete .vscode directory
    
    Signed-off-by: Ryan Posey <64180489+Bombzone@users.noreply.github.com>
    
    * Add Story modal
    
    * styles for add story
    
    * fix autocomplete
    
    * Delete Button
    
    * cleanup and revert storybutton styles
    
    * table
    
    ---------
    
    Signed-off-by: Ryan Posey <64180489+Bombzone@users.noreply.github.com>

--- server/src/js/estimation.tsx
+++ server/src/js/estimation.tsx
@@ -24,14 +24,53 @@ const Estimation = () => { // returns Estimation page
     return (<>
         <header>
             <h1>Got Scrum?</h1>
-            <h5><strong>Team CB's Room<br />ID: 12345</strong></h5>
+            {/* <h5><strong>Team CB's Room<br />ID: 12345</strong></h5> */}
             <NavLink to={"/"}>Leave</NavLink>
         </header>
         <CurrentQueue storyQueue={storyQueue} cards={cards} />
         <StQueue storyQueue={storyQueue} />
         <Estimations estimations={estimations} />
-        <div id="bottomLine"></div>
+        {/* <div id="bottomLine"></div> */}
     </>)
+}
+const Player = (props: { name: string, id: string, points: string}) => {
+    return (
+        <div className="player" id={props.id}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+            </svg>
+            {props.name}:<br />
+            {props.points}
+        </div>
+    )
+}
+const Table = () => {
+    let points: number[] = []
+    let totalPoints: number = 0
+    let numPoints: number = 0
+    points.forEach((point) => {
+        totalPoints += point;
+        numPoints++;
+    })
+    let average;
+    if (numPoints > 0) {
+        average = totalPoints / numPoints
+    }
+    let players: string[][] = [["Player 1", ""], ["Player 2",""], ["Player 3",""], ["Player 4",""], ["Player 5",""], ["Player 6",""]]
+    return (
+        <>
+            <h4 id="avg">{average}</h4>
+            <div id="players">
+                <Player name={players[0][0]} points={players[0][1]} id="player1" />
+                <Player name={players[1][0]} points={players[1][1]} id="player2" />
+                <Player name={players[2][0]} points={players[2][1]} id="player3" />
+                <Player name={players[3][0]} points={players[3][1]} id="player4" />
+                <Player name={players[4][0]} points={players[4][1]} id="player5" />
+                <Player name={players[5][0]} points={players[5][1]} id="player6" />
+            </div>
+
+        </>
+    )
 }
 const CurrentQueue = (props: { storyQueue: UserStoryQueue; cards: Cards }) => { // returns middle section of Estimation page
     const [currentCards, setCards] = React.useState(props.cards);
@@ -65,7 +104,7 @@ const CurrentQueue = (props: { storyQueue: UserStoryQueue; cards: Cards }) => { 
                 <h3>Now estimating:</h3>
                 <h4 className="deleteParent"><Story story={currentStory} list={false} /></h4>
             </div>
-            <h4>AVG</h4>
+            <Table />
             <ul>
                 <ListCards />
             </ul>
@@ -82,9 +121,9 @@ const StoryDialogBox = () => {
                 let story = new UserStory(name);
                 story.setDescription(description);
                 fetch.post("storyQueue", story)
-                }}>
-                <input placeholder="Enter Story Name Here" name="storyName" id="storyName" onChange={event => {name = event.target.value}} required></input>
-                <textarea placeholder="Enter Story Description Here" name="description" id="storyDesc" onChange={event => {description = event.target.value}}></textarea>
+            }}>
+                <input placeholder="Enter Story Name Here" name="storyName" id="storyName" onChange={event => { name = event.target.value }} required></input>
+                <textarea placeholder="Enter Story Description Here" name="description" id="storyDesc" onChange={event => { description = event.target.value }}></textarea>
                 <button type="submit" id="finishedButton">Submit</button>
             </form>
         </Popup>
@@ -137,16 +176,16 @@ const Story = (props: { story: UserStory | undefined; list: boolean }) => { // r
                 <li className="deleteParent">
                     {story.toString()}
                     <button className="liStory" onClick={deleteStory}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-</svg></button>
+                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                    </svg></button>
                 </li>
             )
         } else {
             return (<>
                 {story.toString()}
                 <button id="currentDelete" onClick={deleteStory}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-</svg></button>
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                </svg></button>
             </>)
         }
     }
@@ -159,7 +198,7 @@ const Estimations = (props: { estimations: UserStoryQueue }) => { // returns alr
         if (value <= 3) return "story-small";
         if (value <= 5) return "story-medium";
         return "story-large";
-    }    
+    }
     const [currentEstimations, setEstimations] = React.useState(props.estimations);
     useEffect(() => { // gets estimated stories
         fetch.get("estimations").then((response) => {
